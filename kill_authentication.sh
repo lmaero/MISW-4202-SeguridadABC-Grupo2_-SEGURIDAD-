e3485359a7a8e1fc6a703e10c9cc6b3c10987cb8
diego.eslava <d.eslava@uniandes.edu.co>
2022-09-10 13:03:52 -0500

    Se agrega el script para dar de baja el servicio authentication

---
+++ kill_authentication.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mac='Darwin'
+os_name=$(uname)
+
+# Obtener el tipo de sistema operativo
+if [[ ${os_name} == ${mac} ]]; then
+  process=$(ps aux | grep -i --color "flask run -p 5001" | cut '-d ' -f 7- | cut -d ' ' -f 1)
+else
+  process=$(ps aux | grep -i --color "flask run -p 5001" | cut '-d ' -f 6- | cut -d ' ' -f 1)
+fi
+
+# Extrae el process id de cada microservicio y lo termina
+for i in ${process}:; do
+  if [[ ${i} == *":"* ]]; then
+    process_id=$(echo ${i} | cut -d ':' -f 1)
+    echo "Process id killed ${process_id}"
+    kill ${process_id}
+  else
+    echo "Process id killed ${i}"
+    kill ${i}
+  fi
+done

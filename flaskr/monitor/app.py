0377ce7db29c0cc6ee5a5aeb535aa961d1470553
Luis Guzman <lm.guzmanp1@uniandes.edu.co>
2022-09-10 12:51:41 -0500

    Add try/except/finally block for Monitor component

--- flaskr/monitor/app.py
+++ flaskr/monitor/app.py
@@ -1,5 +1,3 @@
-from cmath import e
-
 import requests
 from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
@@ -12,37 +10,61 @@ jwt = JWTManager(app)
 api = Api(app)
 
 
+class Bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class VistaMonitor(Resource):
     def get(self):
+        services = {}
+
         try:
             print('---------------------------------------------------------------')
-            response_auth = requests.post(f"http://127.0.0.1:5001/auth/login",
-                                          json={"usuario": "monitor"})
+            print(Bcolors.WARNING + "AUTHENTICATION MICROSERVICE" + Bcolors.ENDC)
+
+            response_auth = requests.post(f"http://127.0.0.1:5001/auth/login", json={"usuario": "monitor"})
             print('Authentication Microservice Status:', response_auth.status_code)
             print('Authentication Microservice Content:', response_auth.content)
+            services["Authentication Microservice Status"] = response_auth.status_code
+        except BaseException as error:
+            services["Authentication Microservice Status"] = 503
+            print("Authentication microservice is not available\n", error)
 
+        try:
             print('---------------------------------------------------------------')
+            print(Bcolors.WARNING + "NOTIFICATION MICROSERVICE" + Bcolors.ENDC)
+
             response_notification = requests.post(f"http://127.0.0.1:5002/notification/send",
                                                   json={"alerta_tipo": "ALERTA", "alerta_msg": "¡ALERTA!"})
             print('Notification Microservice Status:', response_notification.status_code)
             print('Notification Microservice Content:', response_notification.content)
+            services["Notification Microservice Status"] = response_notification.status_code
+        except BaseException as error:
+            print("Notification microservice is not available\n", error)
 
+        try:
             print('---------------------------------------------------------------')
-            response_signal_checker = requests.post(f"http://127.0.0.1:5004/signal/checker",
-                                                    json={"signal": True})
+            print(Bcolors.WARNING + "SIGNAL CHECKER MICROSERVICE" + Bcolors.ENDC)
+
+            response_signal_checker = requests.post(f"http://127.0.0.1:5004/signal/checker", json={"signal": True})
             print('SignalChecker Microservice Status:', response_signal_checker.status_code)
             print('SignalChecker Microservice Content:', response_signal_checker.content)
+            services["SignalChecker Microservice Status"] = response_signal_checker.status_code
+        except BaseException as error:
+            print("SignalChecker microservice is not available\n", error)
 
+        finally:
             print('---------------------------------------------------------------')
-            services = {"Authentication Microservice Status": response_auth.status_code,
-                        "Notification Microservice Status": response_notification.status_code,
-                        "SignalChecker Microservice Status:": response_signal_checker.status_code,
-                        }
             json_services = jsonify(services)
-
             return json_services
-        except print('Error en el microservicio monitor:', e):
-            pass
 
 
 api.add_resource(VistaMonitor, '/monitor/check_services')

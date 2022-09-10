00476b2f233b52f1f559faa6b2cfa0fc5013a77a
Alonso Cantu <alonsocantu@cantu.huawei.net>
2022-09-10 12:11:05 -0700

    Add validation to verify the OS type

--- kill_microservices.sh
+++ kill_microservices.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-process=$(ps aux | grep -i --color flask | cut '-d ' -f 7- | cut -d ' ' -f 1)
+mac='Darwin'
+os_name=`uname`
+
+if [[ ${os_name} == ${mac} ]];
+then
+    process=$(ps aux | grep -i --color flask | cut '-d ' -f 7- | cut -d ' ' -f 1)
+else
+    process=$(ps aux | grep -i --color flask | cut '-d ' -f 6- | cut -d ' ' -f 1)
+fi
 
 for i in ${process}:; do
     if [[ ${i} == *":"* ]];

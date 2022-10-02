53ab309dd025510895e73383c951496950c72594
Camilo Gálvez <c.galvezv@uniandes.edu.co>
2022-10-02 14:19:50 -0300

    Se implementa lógica para decodificar token recibido y obtener el usuario asociado

--- flaskr/sensor/app.py
+++ flaskr/sensor/app.py
@@ -1,21 +1,30 @@
 import random
 from datetime import datetime
 
-from flask import Flask
-from flask_jwt_extended import JWTManager
+import jwt
+from flask import Flask, request
+from flask_jwt_extended import jwt_required, JWTManager
 from flask_restful import Api, Resource
 
 import general_queue as gq
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config["JWT_SECRET_KEY"] = "este_secreto_no_debe_de_saberse"  # Change this!
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
-jwt = JWTManager(app)
+jwt_manager = JWTManager(app)
 api = Api(app)
 
 
 class VistaSensor(Resource):
+    @jwt_required()
     def post(self):
+        auth_header_token = request.headers["Authorization"].split(" ")[1]
+        decoded_jwt = jwt.decode(
+                            jwt=auth_header_token,
+                            algorithms=['HS256'],
+                            key="este_secreto_no_debe_de_saberse")
+        user = decoded_jwt["sub"]
+        print(user)
         sensor_criticality = random.randint(0, 5)
         gq.receive_sensor(sensor_criticality)
         gq.new_log_signal("Señal recibida del sensor con criticidad: {}".format(sensor_criticality), datetime.utcnow())
